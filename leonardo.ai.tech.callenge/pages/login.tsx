72597cb8f92674267096ca91c3ff327ea7f81d27
Ting <ting.yun.career@gmail.com>
2024-02-15 20:42:41 -0800

    login redirect

--- leonardo.ai.tech.callenge/pages/login.tsx
+++ leonardo.ai.tech.callenge/pages/login.tsx
@@ -1,6 +1,26 @@
 import LoginButton from "@/components/Login/LoginButton";
+import { getSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Login() {
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (session) {
+        router.replace("/");
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, [router]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <>
       <LoginButton />
