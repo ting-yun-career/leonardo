1dce4c0a9d17ddae4c69eea97839783650e5ab97
Ting <ting.yun.career@gmail.com>
2024-02-17 09:39:29 -0800

    clean

--- leonardo.ai.tech.callenge/pages/index.tsx
+++ leonardo.ai.tech.callenge/pages/index.tsx
@@ -1,13 +1,6 @@
 import Head from "next/head";
-import { Inter } from "next/font/google";
-import { useContext, useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import { getSession } from "next-auth/react";
-import { UserContext } from "@/context/UserContext";
 import HomeComp from "@/components/Home/Home";
 import useAuth from "@/hooks/isAuth";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Root() {
   const { isLoading } = useAuth();
