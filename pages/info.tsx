e8e369abaa159a77fef4e4e2ee70ea840fd2a511
Ting <ting.yun.career@gmail.com>
2024-02-17 22:54:53 -0800

    test

--- pages/info.tsx
+++ pages/info.tsx
@@ -22,22 +22,22 @@ export default function PageInfo(props: Props) {
 }
 
 export async function getStaticProps() {
-  let url;
+  // let url;
 
-  console.log(process.env.VERCEL_URL);
+  // console.log(process.env.VERCEL_URL);
 
-  if (process.env.VERCEL_URL) {
-    url = `${process.env.VERCEL_URL}/api/info`;
-  } else {
-    url = `http://localhost:3000/api/info`;
-  }
-  const res = await fetch(url);
+  // if (process.env.VERCEL_URL) {
+  //   url = `${process.env.VERCEL_URL}/api/info`;
+  // } else {
+  //   url = `http://localhost:3000/api/info`;
+  // }
+  // const res = await fetch(url);
 
-  const payload = await res.json();
+  // const payload = await res.json();
 
   return {
     props: {
-      payload,
+      payload: {},
     },
   };
 }
